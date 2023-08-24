ddf1299d7ca53b330c3a2c3e8f6ea551469365cf
Zeionn <87084175+YukoKatsuki@users.noreply.github.com>
2023-08-24 19:29:39 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 img = "";
-statuss = "";
+status = "";
+objects = [];
 
 function preload(){
     img = loadImage('dog_cat.jpg');
@@ -14,6 +15,19 @@ function setup() {
 
 function draw() {
     image(img, 0, 0, 640, 420);
+
+    if(status != "")
+    {
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("srarus").innerHTML = "Status: Objeto Detectado";
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
     fill("#FF0000");
     text("Dog", 45, 75)
     noFill();
@@ -29,7 +43,7 @@ function draw() {
 
 function modelLoaded() {
     console.log("Model Loaded!")
-    statuss = true;
+    status = true;
     objectDetector.detect(img, gotResult);
 }
 
@@ -38,4 +52,5 @@ function gotResult(error, results) {
         console.log(error);
     }
     console.log(results);
+    objects = results;
 }
