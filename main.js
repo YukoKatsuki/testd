375e374e4838488437302084e5dc40abf1d6c0af
Zeionn <87084175+YukoKatsuki@users.noreply.github.com>
2023-08-24 19:52:16 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function preload(){
 function setup() {
     canvas = createCanvas(640, 420);
     canvas.center();
-    objectDetector = ml5.objectDetector('cocossd, modelLoaded');
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
 
@@ -19,7 +19,7 @@ function draw() {
     if(status != "")
     {
         for (i = 0; i < objects.length; i++) {
-            document.getElementById("srarus").innerHTML = "Status: Objeto Detectado";
+            document.getElementById("status").innerHTML = "Status: Objeto Detectado";
             fill("#FF0000");
             percent = floor(objects[i].confidence * 100);
             text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
@@ -28,17 +28,6 @@ function draw() {
             rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
         }
     }
-    fill("#FF0000");
-    text("Dog", 45, 75)
-    noFill();
-    stroke("#FF0000");
-    rect(30, 60, 450, 350);
-
-    fill ("#FF0000");
-    text("Cat", 320, 120);
-    noFill();
-    stroke("#FF0000");
-    rect(300, 90, 270, 320);
 }
 
 function modelLoaded() {
