6ba4f8d33b8e8a4de0cb5b403d2bad1db8d88b7a
Zeionn <87084175+YukoKatsuki@users.noreply.github.com>
2023-08-29 19:38:59 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -7,25 +7,32 @@ function preload(){
 }
 
 function setup() {
-    canvas = createCanvas(640, 420);
+    canvas = createCanvas(380, 380);
     canvas.center();
-    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
-    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    video = createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
 }
 
 function draw() {
-    image(img, 0, 0, 640, 420);
+    image(video, 0, 0, 380, 380);
 
     if(status != "")
     {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        objectDetector.detect(video, gotResult);
         for (i = 0; i < objects.length; i++) {
             document.getElementById("status").innerHTML = "Status: Objeto Detectado";
-            fill("#FF0000");
+            document.getElementById("numberOfObjects").innerHTML = "Quantidade de Objetos Detectados: "+ objects.length; 
+
+            fill(r,g,b);
             percent = floor(objects[i].confidence * 100);
             text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
             noFill();
-            stroke("#FF0000");
-            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            stroke(r,g,b);
+            rect(objects[i].x - 120, objects[i].y - 80, objects[i].width, objects[i].height);
         }
     }
 }
@@ -33,7 +40,7 @@ function draw() {
 function modelLoaded() {
     console.log("Model Loaded!")
     status = true;
-    objectDetector.detect(img, gotResult);
+    objectDetector.detect(video, gotResult);
 }
 
 function gotResult(error, results) {
@@ -42,4 +49,9 @@ function gotResult(error, results) {
     }
     console.log(results);
     objects = results;
+}
+
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
